cea88417bedc82aab94f47a57ff86d904fd4ce2e
zwtty <59119205+zwtty@users.noreply.github.com>
2019-12-29 07:02:20 -0800

    add index.html
    add index.html

---
+++ static/bdtj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?88d7e7189bdfc910d12d8144c567ced3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
